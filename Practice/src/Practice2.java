21b6468ef1d830153318c8c42a11aba6f2105ad7
sistas <sistas@T60844T3XFB3.som.ad.state.mi.us>
2022-08-19 09:03:08 -0400

    First Commit

---
+++ Practice/src/Practice2.java
@@ -0,0 +1,25 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		arrayList.add(2);
+		arrayList.add(3);
+		arrayList.add(4);
+		arrayList.add(5);
+		arrayList.add(6);
+		arrayList.add(7);
+		BigDecimal age = BigDecimal.ZERO;
+		
+		for(Integer array : arrayList) {
+			age = age.add(BigDecimal.ONE);
+			System.out.println(age);
+			System.out.println("value --->" + new BigDecimal(12.678).setScale(1,RoundingMode.HALF_UP));
+			System.out.println(Math.round(12.38));
+		}
+	}
+}
