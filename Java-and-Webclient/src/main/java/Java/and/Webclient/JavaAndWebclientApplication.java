9628a4a28e655e9a63ed6657d0551b2e3f90d236
sistas <sistas@T60844T3XFB3.som.ad.state.mi.us>
2022-08-20 08:04:14 -0400

    Initial Commit

---
+++ Java-and-Webclient/src/main/java/Java/and/Webclient/JavaAndWebclientApplication.java
@@ -0,0 +1,13 @@
+package Java.and.Webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAndWebclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAndWebclientApplication.class, args);
+	}
+
+}
